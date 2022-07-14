72a372bdcec5007e9a2c436676100b0d7b9f072f
nuhman-bititude <nuhman@bititude.com>
2022-07-14 17:55:43 +0530

    Axios interceptors

--- src/Services/book.js
+++ src/Services/book.js
@@ -1,13 +1,9 @@
 import axios from "./api";
 
 const fetchAll = () => axios.get("/books");
-
 const fetchOne = (data) => axios.get(`/book/${data.id}`, data);
-
 const createBook = (data) => axios.post("/book/create", data);
-
 const deleteBook = (data) => axios.post(`/book/delete/${data.id}`, data);
-
 const updateBook = (data) => axios.post(`/book/update/${data.id}`, data);
 
 export { fetchAll, createBook, deleteBook, updateBook, fetchOne };
--- src/Services/genre.js
+++ src/Services/genre.js
@@ -1,13 +1,9 @@
 import axios from "./api";
 
 const fetchAll = () => axios.get("/genres");
-
 const fetchOne = (data) => axios.get(`/genre/${data.id}`, data);
-
 const createGenre = (data) => axios.post("/genre/create", data);
-
 const deleteGenre = (data) => axios.post(`/genre/delete/${data.id}`, data);
-
 const updateGenre = (data) => axios.post(`/genre/update/${data.id}`, data);
 
 export { fetchAll, createGenre, deleteGenre, updateGenre, fetchOne };
